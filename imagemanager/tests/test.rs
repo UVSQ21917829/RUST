3d86070d0b32567d292fa221772f06be6e7685c7
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-04-16 12:41:38 +0200

    test new

--- imagemanager/tests/test.rs
+++ imagemanager/tests/test.rs
@@ -1,9 +1,13 @@
 
 extern crate imagemanager;
-use imagemanager::image;
 
 #[cfg(test)]
 mod tests {  
-    
-    
+    #[test]
+    fn test_new() {
+        let path = "info.JPG";
+        let meta = imagemanager::image::MetadataImage::new(&path);
+        assert_eq!(meta.image, "info.JPG");
+    }
+      
 } 
