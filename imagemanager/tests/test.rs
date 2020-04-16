bae29aaf364a71c4578e6bd74c113c13b5b5f9aa
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-04-16 12:44:47 +0200

    test pour get_image_date_time

--- imagemanager/tests/test.rs
+++ imagemanager/tests/test.rs
@@ -9,5 +9,10 @@ mod tests {
         let meta = imagemanager::image::MetadataImage::new(&path);
         assert_eq!(meta.image, "info.JPG");
     }
-      
+    #[test]
+    fn test_date() {
+        let path = "info.JPG";
+        let meta = imagemanager::image::MetadataImage::new(&path);
+        assert_eq!(meta.get_image_date_time(), "2017:05:10 10:30:25");
+    }
 } 
