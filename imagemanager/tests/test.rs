fe4a59e9fdf0228dcfe285b08798aa715284fa71
uvsq21807248 <atmanihajar09@gmail.com>
2020-04-21 23:06:46 +0200

    ajout d'un test pour la fonction supports_xmp_fn

--- imagemanager/tests/test.rs
+++ imagemanager/tests/test.rs
@@ -45,4 +45,10 @@ mod tests {
         let meta = imagemanager::image::MetadataImage::new(&path);
         assert_eq!(meta.supports_iptc_fn(),true );
     }
+        #[test]
+    fn test_supports_xmp_fn() {
+        let path = "images/info.JPG";
+        let meta = imagemanager::image::MetadataImage::new(&path);
+        assert_eq!(meta.supports_xmp_fn(),true );
+    }
 } 
